f8f50e947360332e37c3e8c0c6c79b6d50e9fdb5
Ben Blewitt <benrwb@googlemail.com>
2021-02-01 16:17:09 +0000

    remove old copy button; other layout changes

--- workoutCalc.js
+++ workoutCalc.js
@@ -44,16 +44,17 @@ export default {
 
             </div>
 
-            <button v-for="(exercise, idx) in exercises"
-                    v-on:click="gotoPage(idx)"
-                    class="pagebtn"
-                    v-bind:class="{ activeBtn: curPageIdx == idx }">
-                {{ idx + 1 }}
-            </button>
+            <div style="display: inline-block; min-width: 298px">
+                <button v-for="(exercise, idx) in exercises"
+                        v-on:click="gotoPage(idx)"
+                        class="pagebtn"
+                        v-bind:class="{ activeBtn: curPageIdx == idx }">
+                    {{ idx + 1 }}
+                </button>
+                <button v-on:click="addExercise">+</button>
+            </div>
 
-            <button v-on:click="addExercise">+</button>
-
-            <button style="padding: 8.8px 3px 9.5px 3px; margin-left: 88px; margin-right: 5px"
+            <button style="padding: 8.8px 3px 9.5px 3px; margin-right: 5px"
                     v-on:click="copyWorkoutToClipboard"
                     >📋</button><button 
                     class="clearbtn" v-on:click="clearAll">Clear</button>
@@ -64,9 +65,9 @@ export default {
                 <div style="margin-top: 15px; margin-bottom: 10px">
                     <b>Exercise #{{ exIdx + 1 }}:</b>
                     <input type="text" v-model="exercise.name" autocapitalize="off" 
-                           style="width: 225px; border-right-width: 0"
-                    /><button style="vertical-align: top; border: solid 1px #a9a9a9; height: 29px"
-                              v-on:click="copyExerciseToClipboard(exercise)">📋</button>
+                           style="width: 225px"
+                    /><!-- border-right-width: 0 --><!--<button style="vertical-align: top; border: solid 1px #a9a9a9; height: 29px"
+                              v-on:click="copyExerciseToClipboard(exercise)">📋</button>-->
                 </div>
 
                 <div v-if="show1RM && showRmTable""
@@ -341,14 +342,14 @@ export default {
 
             this.workoutDate = moment().format("YYYY-MM-DD"); // update workout date
         },
-        copyExerciseToClipboard: function(exercise) {
-            var text = _generateExerciseText(exercise);
-            navigator.clipboard.writeText(text).then(function() {
-                //alert("success");
-            }, function() {
-                alert("failed to copy");
-            });
-        },
+        //copyExerciseToClipboard: function(exercise) {
+        //    var text = _generateExerciseText(exercise);
+        //    navigator.clipboard.writeText(text).then(function() {
+        //        //alert("success");
+        //    }, function() {
+        //        alert("failed to copy");
+        //    });
+        //},
         copyWorkoutToClipboard: function() {
             var text = this.outputText;
             navigator.clipboard.writeText(text).then(function() {
